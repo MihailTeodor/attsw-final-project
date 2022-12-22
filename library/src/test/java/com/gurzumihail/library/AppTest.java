91596af13d1b6cf6b710f4b2e40d7cbb16e5cacf
mihail <mihail.gurzu@gmail.com>
2022-12-22 12:16:35 +0100

    added failing test

---
+++ library/src/test/java/com/gurzumihail/library/AppTest.java
@@ -0,0 +1,14 @@
+package com.gurzumihail.library;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void test() {
+		assertEquals("Hello", App.main("Hello!"));
+	}
+
+}
